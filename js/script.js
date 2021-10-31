44219447290b9fbc785f3f62915cab239c1f4e60
irisdordio <meavium@gmail.com>
2021-10-31 22:12:36 +0000

    Structure update
    -Desktop and Small Desktop finished.
    
    -Small Desktop redesigned.
    
    -To finish Tablet and Mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//DRAG OBJECTS
 dragElement(document.getElementById("main-drag"));
 
 function dragElement(elmnt) {
@@ -43,28 +42,17 @@ function dragElement(elmnt) {
 }
 
 window.onload = function () {
-    // find the element that you want to drag.
+
     var box = document.getElementById('main-drag');
 
-    /* listen to the touchMove event,
-    every time it fires, grab the location
-    of touch and assign it to box */
-
     box.addEventListener('touchmove', function (e) {
-        // grab the location of touch
         var touchLocation = e.targetTouches[0];
 
-        // assign box new coordinates based on the touch.
         box.style.left = touchLocation.pageX + 'px';
         box.style.top = touchLocation.pageY + 'px';
     })
 
-    /* record the position of the touch
-    when released using touchend event.
-    This will be the drop position. */
-
     box.addEventListener('touchend', function (e) {
-        // current box position.
         var x = parseInt(box.style.left);
         var y = parseInt(box.style.top);
     })
